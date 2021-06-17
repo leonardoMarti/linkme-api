bc2be4cb5e5fde3dfa2c53b9adc38de4cae370c0
Leonardo Martinelli <martinelli087@gmail.com>
2021-06-16 23:08:21 -0300

    validate fields with Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,18 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    const isValid = await schema.isValid(req.body);
+    if (!isValid) return res.status(400).json({ error: 'Validation fails' });
+
     const userExist = await User.findOne({ where: { email: req.body.email } });
     if (userExist)
       return res.status(400).json({ error: 'User already exists!' });
@@ -12,12 +23,30 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    const isValid = await schema.isValid(req.body);
+    if (!isValid) return res.status(400).json({ error: 'Validation fails' });
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
 
     if (email && email !== user.email) {
       const userExist = await User.findOne({ where: { email } });
+
       if (userExist) {
         return res
           .status(400)
@@ -25,9 +54,9 @@ class UserController {
       }
     }
 
-    const nonMatchPassword = !(await user.checkPassword(oldPassword));
-
-    if (oldPassword && nonMatchPassword) {
+    const nonMatchPassword =
+      oldPassword && !(await user.checkPassword(oldPassword));
+    if (nonMatchPassword) {
       return res.status(401).json({ error: 'Old password does not match' });
     }
 
--- src/routes.js
+++ src/routes.js
@@ -8,10 +8,11 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.put('/users', UserController.update);
+
+routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.post('/sessions', SessionController.store);
+routes.put('/users', UserController.update);
 
 export default routes;
